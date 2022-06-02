51a531d3285f0e7b670ffdbd5d2e67255d3f0cd2
NogiTamu <76116273+NogiTamu@users.noreply.github.com>
2022-06-02 19:36:25 +0900

    App

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,29 @@ import "./styles.css";
 import React from "react";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="Todo入力" />
+        <button>追加</button>
+      </div>
+
+      <div>
+        <p>未完了</p>
+        <ul>
+          <li>アナルファック</li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+      </div>
+
+      <div>
+        <p>完了</p>
+        <ul>
+          <li>アナルファック</li>
+          <button>戻す</button>
+        </ul>
+      </div>
+    </>
+  );
 };
