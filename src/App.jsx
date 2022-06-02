78ffe7c5592baefe963d00c1b11a115e22f62e9c
NogiTamu <76116273+NogiTamu@users.noreply.github.com>
2022-06-02 20:50:05 +0900

    未完了機能追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,62 @@
 import "./styles.css";
-import React from "react";
+import React, { useState } from "react";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
+  const [incompTodos, setIncompTodos] = useState(["a", "b"]);
+  const [compTodos, setcompTodos] = useState(["c"]);
+  const onChangeTodoText = (e) => {
+    setTodoText(e.target.value);
+  };
+  const onClickAdd = () => {
+    if (todoText !== "") {
+      const newTodos = [...incompTodos, todoText];
+      setIncompTodos(newTodos);
+      setTodoText("");
+    } else {
+      return;
+    }
+  };
+
   return (
     <>
-      <div>
-        <input placeholder="Todo入力" />
-        <button>追加</button>
+      <div className="input-area">
+        <input
+          placeholder="Todo入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
 
-      <div>
-        <p>未完了</p>
-        <ul>
-          <li>アナルファック</li>
-          <button>完了</button>
-          <button>削除</button>
-        </ul>
+      <div className="incomplete-area">
+        <p className="title">未完了</p>
+
+        {incompTodos.map((todo) => {
+          return (
+            <>
+              <ul key={todo} className="todo-li">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </ul>
+            </>
+          );
+        })}
       </div>
 
-      <div>
-        <p>完了</p>
-        <ul>
-          <li>アナルファック</li>
-          <button>戻す</button>
-        </ul>
+      <div className="complete-area">
+        <p className="title">完了</p>
+        {compTodos.map((todo) => {
+          return (
+            <>
+              <ul key={todo} className="todo-li">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </ul>
+            </>
+          );
+        })}
       </div>
     </>
   );
